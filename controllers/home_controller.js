c66b6547f681bbed178cafd84dd3fc69fd4b9126
protyush1999 <protyushn@gmail.com>
2022-01-04 02:28:46 +0530

    setting up the views engine

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,5 +2,8 @@
 // module.exports.actionName = function(req,res) {};
 
 module.exports.home = function(req,res) {
-    return res.end('<h1>Express is up for Socialley-X</h1>');
+    //return res.end('<h1>Express is up for Socialley-X</h1>');
+    return res.render('home', {
+        title: 'Home Page'
+    });
 };
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 8000;
 
 app.use('/', require('./routes'));
 
+//setting up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err) {
     if(err) {
         console.log(`Error in setting up the server: ${err}`);
